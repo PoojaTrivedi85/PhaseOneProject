ad30ebc8b1d2539c514e7d6d068d16e28897c26c
PoojaTrivedi85 <41534709+PoojaTrivedi85@users.noreply.github.com>
2020-07-01 20:15:28 +0530

    Update FindFile.java

--- FindFile.java
+++ FindFile.java
@@ -6,15 +6,12 @@ public class FindFile {
 	public void findFile(String name,File file)
     {
 		
-		try {
-		 File[] list = file.listFiles();
+	try {
+	 File[] list = file.listFiles();
       
         for (File fil : list)
         {
-          // if (fil.isDirectory())   // This line recursively search all the folders if there is any in the given particular path
-            //{                             // and prints FNF for every directory searched.
-              // findFile(name,fil);  
-            //}
+          
            if (name.equals(fil.getName()))   
             {
                 System.out.println("File found in: " + fil.getParentFile());
